95f13c3af8caec5d92124a39163600e2a0eac8f6
Deniz.Demirsoy <deniz00726@gmail.com>
2023-08-30 23:23:16 +0600

    add tailwind and daisy ui, fix header window error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Footer } from "@/components/Footer";
-import Header from "@/components/Header/Header";
+import dynamic from "next/dynamic";
+
+const Header = dynamic(() => import("@/components/Header/Header"), {ssr:false})
 
 export const metadata: Metadata = {
   title: "ТОО TDK Commerce SA",
