01a84d6d42c770003e9306f8dea2b833e82fba74
Deniz.Demirsoy <deniz.demirsoy@tele2.kz>
2023-09-09 01:35:05 +0600

    add hover animations, css changes

--- src/components/CategoriesFilter/CategoriesFilterItem.tsx
+++ src/components/CategoriesFilter/CategoriesFilterItem.tsx
@@ -1,51 +1,54 @@
 "use client";
 
-import { FC, useId } from "react";
+import {FC, useId} from "react";
 
 import Link from "next/link";
-import { Category } from "@/models/category";
+import {Category} from "@/models/category";
 
 interface Props extends Category {
-  isSelectedCategory: boolean;
-  selectedSubcategory: string;
+    isSelectedCategory: boolean;
+    selectedSubcategory: string;
 }
 
 const CategoriesFilterItem: FC<Props> = ({
-  name,
-  isSelectedCategory,
-  selectedSubcategory,
-  slug,
-  subcategories,
-}) => {
-  const id = useId();
-  const getLiClass = (categorySlug: string, subcategorySlug: string) => {
-    return isSelectedCategory && selectedSubcategory === subcategorySlug
-      ? "mb-[24px] last:mb-0 text-brand-yellow font-semibold"
-      : "mb-[24px] last:mb-0 text-[#FFFFFF80] hover: hover:font-semibold";
-  };
-  return (
-    <div className="collapse collapse-arrow bg-brand-black px-[32px]">
-      <input
-        type="checkbox"
-        name={`category-collapse-${id}`}
-        defaultChecked={isSelectedCategory}
-      />
-      <div className="collapse-title font-bold after:scale-x-150 after:right-0 after:scale-y-150 after:text-brand-yellow text-[20px] px-0">
-        {name}
-      </div>
-      <div className="collapse-content text-[18px]">
-        <ul>
-          {subcategories.map((subcategory, index) => (
-            <Link href={`/products/${slug}/${subcategory.slug}`} key={index}>
-              <li className={getLiClass(slug, subcategory.slug)}>
-                {subcategory.name}
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+                                             name,
+                                             isSelectedCategory,
+                                             selectedSubcategory,
+                                             slug,
+                                             subcategories,
+                                         }) => {
+    const id = useId();
+    const getLiClass = (categorySlug: string, subcategorySlug: string) => {
+        return isSelectedCategory && selectedSubcategory === subcategorySlug
+            ? "mb-[24px] last:mb-0 text-brand-yellow font-semibold"
+            : "mb-[24px] last:mb-0 text-[#FFFFFF80] hover:text-[#FFFFFF] hover:translate-x-2 transition duration-500 easy-out";
+    };
+
+    const collapseClass = isSelectedCategory ? "collapse collapse-arrow bg-[#1D1D1DD9] px-[32px]" : "collapse collapse-arrow hover:bg-[#232323] bg-[#1D1D1DD9]  px-[32px]"
+    return (
+        <div className={collapseClass}>
+            <input
+                type="checkbox"
+                name={`category-collapse-${id}`}
+                defaultChecked={isSelectedCategory}
+            />
+            <div
+                className="collapse-title font-bold after:scale-x-150 after:right-0 after:scale-y-150 after:text-brand-yellow text-[20px] px-0">
+                {name}
+            </div>
+            <div className="collapse-content text-[18px]">
+                <ul>
+                    {subcategories.map((subcategory, index) => (
+                        <Link href={`/products/${slug}/${subcategory.slug}`} key={index}>
+                            <li className={getLiClass(slug, subcategory.slug)}>
+                                {subcategory.name}
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default CategoriesFilterItem;
--- src/components/SubcategoryCard/SubcategoryCard.tsx
+++ src/components/SubcategoryCard/SubcategoryCard.tsx
@@ -1,35 +1,36 @@
-import { FC } from "react";
+import {FC} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "@/components/Button";
 
 interface Props {
-  image: string;
-  name: string;
-  href: string;
+    image: string;
+    name: string;
+    href: string;
 }
 
-const SubcategoryCard: FC<Props> = ({ image, name, href }) => {
-  return (
-    <div className="card w-full bg-base-100 shadow-xl">
-      <figure>
-        <Image
-          src={image}
-          alt={`Изображение ${name}`}
-          width={580}
-          height={282}
-        />
-      </figure>
-      <div className="card-body gap-[24px] justify-between pb-[40px]">
-        <h2 className="text-center text-white text-[26px]">{name}</h2>
-        <div className="card-actions justify-center">
-          <Link href={href}>
-            <Button>Перейти к продуктам</Button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+const SubcategoryCard: FC<Props> = ({image, name, href}) => {
+    return (
+        <Link href={href}>
+            <div
+                className="card w-full bg-[#232323] shadow-xl hover:bg-[#303030] [&>figure>img]:hover:scale-[103%] [&>figure>img]:transition [&>figure>img]:duration-200">
+                <figure className="rounded-2xl">
+                    <Image
+                        src={image}
+                        alt={`Изображение ${name}`}
+                        width={580}
+                        height={282}
+                    />
+                </figure>
+                <div className="card-body gap-[24px] justify-between pb-[40px]">
+                    <h2 className="text-center text-white text-[26px]">{name}</h2>
+                    <div className="card-actions justify-center">
+                        <Button>Перейти к продуктам</Button>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default SubcategoryCard;
