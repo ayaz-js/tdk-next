f985f56157f484c0c28f5d9adddbdd9c2a572a92
r1ndaman <denabsgg@gmail.com>
2024-02-13 23:21:20 +0600

    fix: change categories request

--- src/app/services/categories.tsx
+++ src/app/services/categories.tsx
@@ -1,11 +1,60 @@
 import { Category } from "@/models/category";
 
 export const getCategories = async (): Promise<Category[]> => {
-  const response = await fetch(`${process.env.APP_URL}/api/categories`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch categories");
-  }
-  return response.json();
+  return [
+    {
+      name: "Дробильное оборудование",
+      slug: "crushing-equipment",
+      subcategories: [
+        {
+          name: "Холодные фрезы",
+          slug: "cold-milling-machines",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+        {
+          name: "Ресайклеры и стабилизаторы",
+          slug: "recyclers-and-stabilizers",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+        {
+          name: "Карьерный комбайн",
+          slug: "quarry-combine",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+        {
+          name: "Асфальтоукладчики",
+          slug: "asphalt-pavers",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+      ],
+    },
+    {
+      name: "Грохота, Классификаторы",
+      slug: "roar-classifiers",
+      subcategories: [
+        {
+          name: "Холодные фрезы",
+          slug: "cold-milling-machines",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+        {
+          name: "Ресайклеры и стабилизаторы",
+          slug: "recyclers-and-stabilizers",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+        {
+          name: "Карьерный комбайн",
+          slug: "quarry-combine",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+        {
+          name: "Асфальтоукладчики",
+          slug: "asphalt-pavers",
+          image: "/images/subcategories/subcategory_1.webp",
+        },
+      ],
+    },
+  ];
 };
 
 export const getCategory = async (
